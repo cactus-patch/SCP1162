4dad7635428df6e7d79d3669688bc4f16f772145
nora <nora@norain.icu>
2024-12-22 22:17:44 -0800

    better way to get instance config

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -4,22 +4,25 @@ using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs.Player;
 using PlayerRoles;
 using SCP1162.API;
+using SCP1162;
+using Debug = System.Diagnostics.Debug;
 using Random = UnityEngine.Random;
 
 namespace SCP1162;
 
-public class EventHandlers(Plugin plugin) {
-  public void OnItemDropped(DroppingItemEventArgs ev) {
+public static class EventHandlers {
+  public static void OnItemDropped(DroppingItemEventArgs ev) {
+    if (Plugin.Instance == null) return;
     if (!ev.IsAllowed) return;
     if (!(Vector3.Distance(ev.Player.Position, RoleTypeId.Scp173.GetRandomSpawnLocation().Position) <= 8.2f)) return;
-    if (plugin.Config.UseHints)
-     ev.Player.ShowHint(plugin.Config.ItemDropMessage, plugin.Config.ItemDropMessageDuration);
+    if (Plugin.Instance.Config.UseHints)
+     ev.Player.ShowHint(Plugin.Instance.Config.ItemDropMessage, Plugin.Instance.Config.ItemDropMessageDuration);
     else
-      ev.Player.Broadcast(plugin.Config.ItemDropMessageDuration, plugin.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
+      ev.Player.Broadcast(Plugin.Instance.Config.ItemDropMessageDuration, Plugin.Instance.Config.ItemDropMessage, Broadcast.BroadcastFlags.Normal, true);
     ev.IsAllowed = false;
     var oldItem = ev.Item.Base.ItemTypeId;
     ev.Player.RemoveItem(ev.Item);
-    var newItemType = plugin.Config.Chances[Random.Range(0, plugin.Config.Chances.Count)];
+    var newItemType = Plugin.Instance.Config.Chances[Random.Range(0, Plugin.Instance.Config.Chances.Count)];
     var eventArgs = new UsingScp1162EventArgs(ev.Player, newItemType, oldItem);
     Scp1162Event.OnUsingScp1162(eventArgs);
     var newItem = Item.Create(eventArgs.ItemAfter);
--- Plugin.cs
+++ Plugin.cs
@@ -5,16 +5,18 @@ namespace SCP1162;
 public class Plugin : Plugin<Config> {
   public override string Name => "SCP1162";
   public override string Author => "xRoier, furry";
-  private EventHandlers EventHandlers => new(this);
-  public override Version Version => new(9, 0, 1);
+  public override Version Version => new(9, 0, 2);
   public override Version RequiredExiledVersion => new(9, 0, 0);
+  public static Plugin? Instance;
 
   public override void OnEnabled() {
+    Instance = this;
     Exiled.Events.Handlers.Player.DroppingItem += EventHandlers.OnItemDropped;
     base.OnEnabled();
   }
 
   public override void OnDisabled() {
+    Instance = null;
     Exiled.Events.Handlers.Player.DroppingItem -= EventHandlers.OnItemDropped;
     base.OnDisabled();
   }
