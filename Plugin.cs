9ea64db641b8324915bd0816ac988afd247469a9
nora <nora@norain.icu>
2024-12-23 17:47:51 -0800

    v9.1.0

--- Plugin.cs
+++ Plugin.cs
@@ -5,7 +5,7 @@ namespace SCP1162;
 public class Plugin : Plugin<Config> {
   public override string Name => "SCP1162";
   public override string Author => "xRoier, furry";
-  public override Version Version => new(9, 0, 2);
+  public override Version Version => new(9, 1, 0);
   public override Version RequiredExiledVersion => new(9, 0, 0);
   private EventHandler? _eventHandler;
   
